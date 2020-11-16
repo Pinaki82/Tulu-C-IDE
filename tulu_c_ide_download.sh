fa5efd9016c3ace4e8fe177bd89ce8386f981c5e
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2020-11-17 00:30:43 +0530

    Update tulu_c_ide_download.sh

--- tulu_c_ide_download.sh
+++ tulu_c_ide_download.sh
@@ -3,10 +3,9 @@
 cd ~/  && \
 git clone https://github.com/Pinaki82/Tulu-C-IDE  && \
 rm -rf ~/Tulu-C-IDE/.git  && \
-rm ~/Tulu-C-IDE/README.md ~/Tulu-C-IDE/LICENSE  && \
+rm ~/Tulu-C-IDE/README.md ~/Tulu-C-IDE/LICENSE ~/Tulu-C-IDE/tulu_c_ide_download.sh && \
 cd ~/Tulu-C-IDE && find ./ -maxdepth 1 -mindepth 1 -exec mv -f -t ~/ {} +  && \
 
 # https://unix.stackexchange.com/questions/50487/how-to-move-all-files-and-folders-via-mv-command
 
 rm -rf ~/Tulu-C-IDE \
-
