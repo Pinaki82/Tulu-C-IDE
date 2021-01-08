66f43fac534e9ef2700964997fa500e18433ed40
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2021-01-08 18:47:11 +0530

    Update update-Tulu-C-IDE.sh

--- .vim/updater/update-Tulu-C-IDE.sh
+++ .vim/updater/update-Tulu-C-IDE.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# ***** WARNING *****
+# Do NOT BLINDLY run this script.
+# READ EVERY LINE before running this script.
+# Make necessary changes if needed.
+# All other files except the files in the directories ~/.vim/plugged/ and
+# ~/.vim/plugin/ will be overwritten by the latest GitHub repository contents.
+# If you have modified the configuration, please keep a backup
+# and merge your customisation from the backup using Meld or VimDiff
+# once the update is done.
+
 cd ~/  && \
 rm -rf ~/Tulu-C-IDE && \
 git clone https://github.com/Pinaki82/Tulu-C-IDE  && \
@@ -9,7 +19,6 @@ rm ~/Tulu-C-IDE/README.md ~/Tulu-C-IDE/LICENSE ~/Tulu-C-IDE/tulu_c_ide_download.
 rsync --archive --recursive --verbose --progress ~/Tulu-C-IDE/.vim/autoload/* ~/.vim/autoload && \
 rsync --archive --recursive --verbose --progress ~/Tulu-C-IDE/.vim/bitmaps/* ~/.vim/bitmaps && \
 rsync --archive --recursive --verbose --progress ~/Tulu-C-IDE/.vim/updater// ~/.vim/updater &&\
-# rsync --archive --recursive --verbose --progress ~/Tulu-C-IDE/.vim/plugin/* ~/.vim/plugin && \
 # https://unix.stackexchange.com/questions/501975/why-does-rsync-keep-skipping-the-directory-i-want-to-sync
 rsync --archive --recursive --verbose --progress ~/Tulu-C-IDE/.vimdotcommon// ~/.vimdotcommon &&\
 rsync --archive --recursive --update --verbose --progress ~/Tulu-C-IDE/.vimdotlinux// ~/.vimdotlinux &&\
