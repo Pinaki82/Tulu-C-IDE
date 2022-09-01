620b7c2c4d2e56c15d86001e295dde9ed6b31d8b
Pinaki <appugupta007@gmail.com>
2022-09-01 22:17:51 +0530

    Updated a Shell file (snippet) that generates 'tags' database.

--- Snippets_n_Extras/snippets/sh/taggen.sh
+++ Snippets_n_Extras/snippets/sh/taggen.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-ctags -R --sort=yes --c-kinds=+pxfvtdeglmsu --c++-kinds=+pxfvtcdeglmnsu --languages=C,C++ --langmap=C:.c.h.ino.pde --langmap=C++:+.tcc..C.h.c.cpp.hpp.c++.cc.cp.cxx.h++.hh.hp.hxx.ino.pde --fields=+iaSmz --extras=+qf -f tags * 
+# https://www.tutorialspoint.com/unix_commands/ctags.htm
+# ctags --list-kinds
+# ctags --list-fields
+# ctags --list-extras
+
+# Ctags will generate tags DB only for function parameters and
+# header file names. Use the old command set if you need a
+# more comprehensive tags database for some reason.
+
+# Old: ctags -R --sort=yes --c-kinds=+pxfvtdeglmsu --c++-kinds=+pxfvtcdeglmnsu --languages=C,C++ --langmap=C:.c.h.ino.pde --langmap=C++:+.tcc..C.h.c.cpp.hpp.c++.cc.cp.cxx.h++.hh.hp.hxx.ino.pde --fields=+iaSmz --extras=+qf -f tags * 
+
+ctags -R --sort=yes --c-kinds=+pfhd --c++-kinds=+pfhd --languages=C,C++ --langmap=C:.c.h.ino.pde --langmap=C++:+.tcc..C.h.c.cpp.hpp.c++.cc.cp.cxx.h++.hh.hp.hxx.ino.pde --fields=+lSmz --extras=+f -f tags * 
