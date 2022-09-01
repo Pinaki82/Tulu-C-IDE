2ac6136816d0812ac9a94a3847448d03fd49ac43
Pinaki <appugupta007@gmail.com>
2022-09-01 22:21:28 +0530

    Updated a DOSBatch file (snippet) that generates 'tags' database.

--- Snippets_n_Extras/snippets/dosbatch/taggen.bat
+++ Snippets_n_Extras/snippets/dosbatch/taggen.bat
@@ -1,2 +1,15 @@
+@ECHO OFF
+REM https://www.tutorialspoint.com/unix_commands/ctags.htm
+REM ctags --list-kinds
+REM ctags --list-fields
+REM ctags --list-extras
+
+REM Ctags will generate tags DB only for function parameters and
+REM header file names. Use the old command set if you need a
+REM more comprehensive tags database for some reason.
+
+REM Old: ctags -R --sort=yes --c-kinds=+pxfvtdeglmsu --c++-kinds=+pxfvtcdeglmnsu --languages=C,C++ --langmap=C:.c.h.ino.pde --langmap=C++:+.tcc..C.h.c.cpp.hpp.c++.cc.cp.cxx.h++.hh.hp.hxx.ino.pde --fields=+iaSmz --extras=+qf -f tags * 
+@ECHO ON
+
 ctags -R --sort=yes --c-kinds=+pxfvtdeglmsu --c++-kinds=+pxfvtcdeglmnsu --languages=C,C++ --langmap=C:.c.h.ino.pde --langmap=C++:+.tcc..C.h.c.cpp.hpp.c++.cc.cp.cxx.h++.hh.hp.hxx.ino.pde --fields=+iaSmz --extras=+qf -f tags *
 cmd
