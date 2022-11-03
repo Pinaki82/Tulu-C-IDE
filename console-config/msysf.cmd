ae45927c7fcca8db35eea21d51949bbf3b4494e9
Pinaki Gupta <appugupta007@gmail.com>
2022-11-03 21:24:09 +0530

    Access MSYS2 from terminal emulators other than the default Mintty.

---
+++ console-config/msysf.cmd
@@ -0,0 +1,43 @@
+@echo off
+setlocal
+
+c:/msys64/usr/bin/fish.exe --login -i
+
+REM Access MSYS2 from terminal emulators
+REM other than the default Mintty, e.g.,
+REM WezTerm, CMD, "AlacrittyTerm", etc.
+REM NOTE: Some features may not be available.
+
+REM Keep this file in
+REM C:\msys64
+REM alongside
+REM msys2.exe.
+
+REM https://github.com/microsoft/terminal/issues/1669
+
+REM WINDOWS+r -> systempropertiesadvanced ->
+REM -> Environment Variables ->
+REM -> System Variables -> Path -> Edit -> New
+
+REM Or, (shortcut): WINDOWS+r -> rundll32.exe sysdm.cpl,EditEnvironmentVariables
+
+REM Add to PATH:
+
+REM C:\msys64\home\AppuRaja\bin;
+REM C:\msys64\usr\bin;
+REM C:\msys64\usr\x86_64-pc-msys\bin;
+REM C:\msys64;
+REM C:\msys64\mingw64\bin;
+REM C:\msys64\opt\node-v16.16.0-win-x64;
+
+REM Also, add
+REM C:\Users\AppuRaja\.cargo\bin;
+REM for accessing cargo package builds.
+
+REM Type
+REM PATH
+REM to see the directories in your PATH Environment Variables.
+
+REM Use: Type
+REM msysf.cmd
+REM Hit Enter
