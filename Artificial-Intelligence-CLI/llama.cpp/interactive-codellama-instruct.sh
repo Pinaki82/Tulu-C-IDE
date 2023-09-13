7444cc671d420e7ab2cefa25c6196200541f6e3c
Pinaki <appugupta007@gmail.com>
2023-09-13 22:14:55 +0530

    Desktop Entries and Bash Scripts for llama.cpp, https://github.com/ggerganov/llama.cpp.

---
+++ Artificial-Intelligence-CLI/llama.cpp/interactive-codellama-instruct.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://github.com/garyexplains/examples/blob/master/how-to-run-llama-cpp-on-raspberry-pi.md
+# https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/tree/main
+# https://huggingface.co/TheBloke/CodeLlama-7B-Instruct-GGUF/tree/main
+
+
+cd /home/YOUR_USERNAME/PortablePrograms/llama.cpp \
+
+./main  -m models/codellama-7b-instruct.Q2_K.gguf --color \
+       --ctx_size 2048 \
+       -n -1 \
+       -ins -b 256 \
+       --top_k 10000 \
+       --temp 0.2 \
+       --repeat_penalty 1.1
