1da6d9eeea38438c442464310de3058b038199c0
Pinaki Gupta <appugupta007@gmail.com>
2023-09-29 19:41:12 +0530

    NEW: Model-runner scripts for LLaMA.CPP.

---
+++ Artificial-Intelligence-CLI/llama.cpp/interactive-Gorilla-7B.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://github.com/garyexplains/examples/blob/master/how-to-run-llama-cpp-on-raspberry-pi.md
+# https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/tree/main
+# https://huggingface.co/TheBloke/CodeLlama-7B-Instruct-GGUF/tree/main
+
+cd /home/appu/PortablePrograms/llama.cpp \
+
+./main  -m models/Gorilla-7B.Q2_K.gguf --color \
+       --ctx_size 2048 \
+       -n -1 \
+       -ins -b 256 \
+       --top_k 10000 \
+       --temp 0.2 \
+       --repeat_penalty 1.1
