afd727140c844cae8f187bf7740f1e246e849ee6
Pinaki <appugupta007@gmail.com>
2023-10-05 13:34:55 +0530

    UPDATE: Entries related to LLaMA.CPP.

--- Artificial-Intelligence-CLI/llama.cpp/interactive-Gorilla-7B.sh
+++ Artificial-Intelligence-CLI/llama.cpp/interactive-Gorilla-7B.sh
@@ -4,7 +4,7 @@
 # https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/tree/main
 # https://huggingface.co/TheBloke/CodeLlama-7B-Instruct-GGUF/tree/main
 
-cd /home/appu/PortablePrograms/llama.cpp \
+cd /home/YOUR_USERNAME/PortablePrograms/llama.cpp \
 
 ./main  -m models/Gorilla-7B.Q2_K.gguf --color \
        --ctx_size 2048 \
